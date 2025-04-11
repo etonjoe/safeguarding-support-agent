4847f76138b0bac07bf532ce3191cbbce3894252
Joseph Eton <5099886+etonjoe@users.noreply.github.com>
2025-04-11 22:53:12 +0100

    Add files via upload

--- safeguarding_logic.py
+++ safeguarding_logic.py
@@ -5,9 +5,8 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain.agents import AgentExecutor, create_tool_calling_agent, tool
-from langchain_core.messages import MessagesPlaceholder
 from langchain.chains import create_retrieval_chain
-from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain.chains.combine_documents import create_stuff_documents_chain
 
 # Function: Load and Split PDF
